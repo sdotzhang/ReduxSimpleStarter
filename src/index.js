c443faf3c4276d36b4c79b4ab7aa82770bfc0c3d
Shaonan Zhang <sdotzhang@gmail.com>
2017-09-14 21:47:17 -0700

    added api key
    Change-Id: I6fa4e3cfe482257a11852120d226833d60306c46

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDQnhNxLfo78AGt-li55LUugipJyJ79TWQ';
 
 // create a new component, this component procedues some html
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (DOM)
